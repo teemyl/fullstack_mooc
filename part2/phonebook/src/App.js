c356fc1a8f1fcc4d25e9ae9d7668cf28c29b1e19
Teemu Myllylä <teemu.myllyla@navitep.com>
2020-06-29 12:15:11 +0300

    phonebook 2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleNameInput = (event) => setNewName(event.target.value)
+  
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(newName)
+    setPersons(persons.concat({ name: newName }))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={ handleSubmit }>
+        <div>
+          name: <input value={ newName } onChange={ handleNameInput }/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {
+        persons.map(person => <div key={ person.name }>{ person.name }</div>)
+      }
+    </div>
+  )
+}
+
+export default App
