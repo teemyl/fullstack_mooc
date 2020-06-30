c39cb4fb2f5a8a9f74994e919788284d3deb72e9
Teemu Myllylä <teemu.myllyla@navitep.com>
2020-06-30 22:11:35 +0300

    fix handler pass as prop to use reference, part2 2.15-6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 
 import { Filter, PersonForm, PersonList } from './Components'
+import personService from './services/persons'
 
 const App = () => {
   const [ persons, setPersons ] = useState([])
@@ -11,11 +11,9 @@ const App = () => {
   const [ filter, setNewFilter ] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
-      })
+    personService
+      .getAll()
+      .then(persons => setPersons(persons))
   }, [])
 
   const handleNameInput = (event) => setNewName(event.target.value)
@@ -29,9 +27,13 @@ const App = () => {
     if (persons.findIndex(obj => obj.name === newName) > -1)
       alert(`${ newName } is already added to phonebook`)
     else {
-      setPersons(persons.concat(newPerson))
-      setNewNumber('')
-      setNewName('')
+      personService
+        .create(newPerson)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          setNewNumber('')
+          setNewName('')
+        })
     }
   }
 
@@ -39,16 +41,16 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
 
-      <Filter filter={ filter } handleFilterInput={ handleFilterInput } />
+      <Filter filter={ filter } handleFilterInput={ (e) => handleFilterInput(e) } />
 
       <h3>add a new</h3>
 
       <PersonForm
         newName = { newName }
         newNumber = { newNumber }
-        handleSubmit = { handleSubmit }
-        handleNameInput = { handleNameInput }
-        handleNumberInput = { handleNumberInput }
+        handleSubmit = { (e) => handleSubmit(e) }
+        handleNameInput = { (e) => handleNameInput(e) }
+        handleNumberInput = { (e) => handleNumberInput(e) }
       />
 
       <h3>Numbers</h3>
