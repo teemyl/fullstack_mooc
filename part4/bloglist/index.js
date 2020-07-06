1ca5a3a4d8d4b9dd528d02718288d1958a02fe8c
Teemu Myllylä <teemu.myllyla@navitep.com>
2020-07-06 11:41:54 +0300

    part4 refactor controllers

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -2,31 +2,14 @@ require('dotenv').config()
 
 const http = require('http')
 const express = require('express')
-const cors = require('cors')
-const Blog = require('./models/blog')
-
 const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = process.env.POST || 3001
 app.listen(PORT, () => {
