50575618a7f7e82faecb32fda091876428cd950f
Teemu Myllylä <teemu.myllyla@navitep.com>
2020-07-08 13:43:25 +0300

    part4 4.18

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose')
 // Router imports
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 // Utils imports
 const middleware = require('./utils/middleware')
@@ -32,6 +33,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 // Apply routes
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
