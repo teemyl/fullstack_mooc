d0232dc3e24a3ece4bedcf7e79316f3ca1774b7a
Teemu Myllylä <teemu.myllyla@navitep.com>
2020-07-08 11:14:02 +0300

    part4 4.13

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('./../models/blog')
 const { result } = require('lodash')
+const blog = require('./../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
@@ -10,7 +11,20 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
   const result = await blog.save()
-  response.json(result.toJSON())
+  response.status(201).json(result.toJSON())
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  console.log(request.params.id)
+  const blogToDelete = await Blog.findById(request.params.id)
+  
+  if (blogToDelete) {
+    await Blog.deleteOne(blogToDelete)
+    response.status(204).end()
+  }
+  else {
+    response.status(404).end()
+  }
 })
 
 module.exports = blogsRouter
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -15,84 +15,118 @@ beforeEach(async () => {
   
   await Promise.all(promiseArr)
 })
-
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
-
-test('correct amount of blogs are returned', async () => {
-  res = await api.get('/api/blogs')
-  expect(res.body).toHaveLength(helper.initialBlogs.length)
-})
-
-test('blog id key is formatted correctly', async () => {
-  res = await api.get('/api/blogs')
-  expect(res.body[0].id).toBeDefined()
-})
-
-test('a valid blog can be added', async () => {
-  newBlog = {
-    title: 'adding a new entry',
-    author: 'jest test suite',
-    url: 'localhost',
-    likes: 1
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('when there\'s initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
   
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-
-  const titles = (blogsAtEnd).map(b => b.title)
-  expect(titles).toContain('adding a new entry')
+  test('correct amount of blogs are returned', async () => {
+    res = await api.get('/api/blogs')
+    expect(res.body).toHaveLength(helper.initialBlogs.length)
+  })
+  
+  test('blog id key is formatted correctly', async () => {
+    res = await api.get('/api/blogs')
+    expect(res.body[0].id).toBeDefined()
+  })
 })
 
-test('adding a blog without likes default likes to zero', async () => {
-  newBlog = {
-    title: 'adding a new entry',
-    author: 'jest test suite',
-    url: 'localhost'
-  }
+describe('adding of a new blog', () => {
+  test('succeeds with valid data', async () => {
+    newBlog = {
+      title: 'adding a new entry',
+      author: 'jest test suite',
+      url: 'localhost',
+      likes: 1
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  
+    const titles = (blogsAtEnd).map(b => b.title)
+    expect(titles).toContain('adding a new entry')
+  })
 
-  const res = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  expect(res.body.likes).toBe(0)
+  test('without likes defaults likes to zero', async () => {
+    newBlog = {
+      title: 'adding a new entry',
+      author: 'jest test suite',
+      url: 'localhost'
+    }
+  
+    const res = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    expect(res.body.likes).toBe(0)
+  })
+  
+  test('without title fails with status code 400', async () => {
+    newBlog = {
+      author: 'jest test suite',
+      url: 'localhost'
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  })
+  
+  test('without url fails with status code 400', async () => {
+    newBlog = {
+      title: 'adding a new entry',
+      author: 'jest test suite'
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  })
 })
 
-test('adding a blog without title fails', async () => {
-  newBlog = {
-    author: 'jest test suite',
-    url: 'localhost'
-  }
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+    await api
+      .delete(`/api/blogs/${ blogToDelete.id }`)
+      .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
+    
+    const titles = blogsAtEnd.map(b => {
+      return {title: b.title, author: b.author}
+    })
+
+    expect(titles).not.toContainEqual(
+      { title: blogToDelete.title, author: blogToDelete.author}
+    )
+  })
+  
+  test('fails with status code 404 if id is invalid', async () => {
+    const nonExistingId = await helper.nonExistingId()
+
+    await api
+      .delete(`/api/blogs/${ nonExistingId }`)
+      .expect(404)
+  })
 })
 
-test('adding a blog without url fails', async () => {
-  newBlog = {
-    title: 'adding a new entry',
-    author: 'jest test suite'
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-})
 
 afterAll(() => {
   mongoose.connection.close()
