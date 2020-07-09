9deee9f8736241431550088a253c6b6a953d07d5
Teemu Myllylä <teemu.myllyla@navitep.com>
2020-07-09 21:25:22 +0300

    part5 5.9

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -150,7 +150,11 @@ const App = () => {
       <Toggleable buttonLabel='add blog' ref={ blogFormRef }>
         <BlogForm createBlog={ addBlog } />
       </Toggleable>
-      { blogs.map(blog => <Blog key={ blog.id } blog={ blog } updateBlog={ updateBlog } />) }
+      {
+        blogs
+          .sort((a, b) => b.likes - a.likes)
+          .map(blog => <Blog key={ blog.id } blog={ blog } updateBlog={ updateBlog } />)
+      }
     </div>
   )
 
