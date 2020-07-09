1952117c60f7426764a7debb857ab82da14d1f56
Teemu Myllylä <teemu.myllyla@navitep.com>
2020-07-09 19:26:54 +0300

    part5 5.7, 5.6 was included in previous commit (woops)

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState} from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+
+  const [viewDetails, setViewDetails] = useState(false)
+
+  const renderDetails = () => (
+    <>
+    { blog.url }<br />
+    likes { blog.likes} <button>like</button><br />
+    { blog.author }
+    </>
+  )
+
+  const blogStyle = {
+    borderStyle: 'solid',
+    borderRadius: 5,
+    borderWidth: 1,
+    marginBottom: 5,
+    padding: 5
+  }
+
+  return (
+    <div style={ blogStyle }>
+      { blog.title }&nbsp;
+      <button onClick={ () => setViewDetails(!viewDetails) }>
+        { viewDetails ? 'hide' : 'view' }
+      </button>
+      <br />
+      { viewDetails && renderDetails() }
+    </div>
+  )
+}
 
 export default Blog
