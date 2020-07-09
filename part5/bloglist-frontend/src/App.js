ae3bbc8207e7c9933549f155b92777440a87a0ee
Teemu Myllylä <teemu.myllyla@navitep.com>
2020-07-09 17:01:56 +0300

    part5 5.1

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +15,76 @@ const App = () => {
     )  
   }, [])
 
-  return (
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    }
+    catch (exception) {
+      
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={ handleLogin }>
+      <div>
+        username
+        <input
+          type='text'
+          value={ username }
+          name="Username"
+          onChange={ ({ target }) => setUsername(target.value) }
+        />
+      </div>
+      <div>
+        password
+        <input
+          type='text'
+          value={ password }
+          name="Username"
+          onChange={ ({ target }) => setPassword(target.value) }
+        />
+      </div>
+      <button type='submit'>login</button>
+    </form>
+  )
+
+  /* const blogForm = () => (
+    <form onSubmit={ addBlog }>
+      <input
+        value={ newNote }
+        onChange={ handleBlogChange }
+      />
+      <button type='submit'>save</button>
+    </form>
+  ) */
+
+  const showBlogs = () => (
     <div>
+      <div>{ user.name } logged in</div>
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+    </div>
+  )
+
+  return (
+    <div>
+      {
+        user === null ?
+        loginForm() :
+        showBlogs()
+      }
+
+      
     </div>
   )
 }
