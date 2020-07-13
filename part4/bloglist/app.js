29e4e425d747edd9a81e85d7d71446902f23404c
Teemu Myllylä <teemu.myllyla@navitep.com>
2020-07-13 14:43:31 +0300

    part5 5.17

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -8,6 +8,7 @@ const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const testingRouter = require('./controllers/testing')
 
 // Utils imports
 const middleware = require('./utils/middleware')
@@ -37,6 +38,11 @@ app.use(middleware.requestLogger)
 app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 // Post-route middleware
 app.use(middleware.unknownEndpoint)
--- part5/bloglist-frontend/.eslintrc.js
+++ part5/bloglist-frontend/.eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   "env": {
       "browser": true,
       "es6": true,
+      "jest/globals": true,
+      "cypress/globals": true
   },
   "extends": [ 
       "eslint:recommended",
@@ -15,7 +17,7 @@ module.exports = {
       "sourceType": "module"
   },
   "plugins": [
-      "react", "jest"
+      "react", "jest", "cypress"
   ],
   "rules": {
       "indent": [
