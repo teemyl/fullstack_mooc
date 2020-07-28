27191c738cb132f24b58aa882debe35cdcbe534f
Teemu Myllylä <teemu.myllyla@navitep.com>
2020-07-28 14:36:23 +0300

    part7 7.6

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -101,15 +101,15 @@ const CreateNew = (props) => {
       <form onSubmit={ handleSubmit }>
         <div>
           content
-          <input { ...content } />
+          <input { ...{ ...content, reset: '' } } />
         </div>
         <div>
           author
-          <input { ...author } />
+          <input { ...{ ...author, reset: '' } } />
         </div>
         <div>
           url for more info
-          <input { ...info } />
+          <input { ...{ ...info, reset: '' } } />
         </div>
         <button>create</button>
         <button onClick={ reset }>reset</button>
