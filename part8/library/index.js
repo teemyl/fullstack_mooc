a632b1691bcea0076f565a0d75187205c26d0055
Teemu Myllylä <teemu.myllyla@navitep.com>
2020-08-06 12:27:33 +0300

    part8  8.4

--- part8/library/index.js
+++ part8/library/index.js
@@ -101,7 +101,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -110,7 +110,11 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      return args.author
+      ? books.filter(b => b.author === args.author)
+      : books
+    },
     allAuthors: () => authors
   },
   Author: {
