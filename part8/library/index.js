8b646d6281dc76ac8ceed813aedda5729c674a51
Teemu Myllylä <teemu.myllyla@navitep.com>
2020-08-06 12:08:35 +0300

    part8 8.2

--- part8/library/index.js
+++ part8/library/index.js
@@ -84,16 +84,27 @@ let books = [
 ]
 
 const typeDefs = gql`
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: String!
+    genres: [String!]!
+  }
+
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
